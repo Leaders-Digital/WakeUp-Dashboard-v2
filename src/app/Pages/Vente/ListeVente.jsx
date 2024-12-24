3e00ecd30a8787beb3f8ad3b17f71672ef12811f
punk010 <mohamed.amine.atouii@gmail.com>
2024-12-24 12:43:35 +0100

    added vante

--- src/app/Pages/Vente/ListeVente.jsx
+++ src/app/Pages/Vente/ListeVente.jsx
@@ -1,273 +1,155 @@
-// src/app/Pages/Products/ProductList.jsx
-import React, { useEffect, useState } from "react";
-import {
-    Table,
-    Tag,
-    Modal,
-    message,
-    Checkbox,
-    Spin,
-    Input,
-    Button,
-    Select,
-    Row,
-    Col,
-    Card,
-    Divider,
-    Space
-} from "antd";
-import axios from "axios";
-import { Breadcrumb } from "app/components";
-import {
-    DeleteOutlined,
-    EditOutlined,
-    ExclamationCircleOutlined,
-    PlusOutlined
-} from "@ant-design/icons";
-import { Box } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-
-const { confirm } = Modal;
-const { Option } = Select;
-
-// Statistics Card Component
-const StatsCard = ({ title, value }) => (
-    <Card title={title} bordered={false}>
-        <h2 style={{ textAlign: "center" }}>{value}</h2>
-    </Card>
-);
-
-// Filters Component
+import React, { useState, useEffect } from 'react';
+import { Table, Spin, Alert, Button, Input, Tag } from 'antd';
+import { EyeOutlined, CheckCircleOutlined, SearchOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import moment from 'moment';
+import { useNavigate } from 'react-router-dom';
 
 const ListeVente = () => {
-    // State Variables
-    const [products, setProducts] = useState([]);
-    const [categories, setCategories] = useState([]);
-    const [categoryFilter, setCategoryFilter] = useState("Tous les catégories");
+    const [ventes, setVentes] = useState([]);
+    const [filteredVentes, setFilteredVentes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     const [search, setSearch] = useState("");
-    const [isSale, setIsSale] = useState(false);
-    const [priceFilter, setPriceFilter] = useState("asc");
-    const [fullResponse, setFullResponse] = useState({});
-    const [loading, setLoading] = useState(false);
-    const [loadingStock, setLoadingStock] = useState(false); // Nouvelle variable d'état
+    const navigate = useNavigate();
 
-
-    const navigate = useNavigate(); // Initialize useNavigate inside the component
-
-
-
-
-
-    // Fetch Products
-
-    // Fetch Categories
-
-
-    // Effect Hooks
-    useEffect(() => {
-        // getProducts();
-    }, [search, categoryFilter, isSale, priceFilter]);
+    const fetchVentes = async () => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_API_URL_PRODUCTION}api/vente`, {
+                headers: { "x-api-key": process.env.REACT_APP_API_KEY },
+            });
+            setVentes(response.data.ventes);
+            setFilteredVentes(response.data.ventes);
+            setLoading(false);
+        } catch (err) {
+            setError(err.message);
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        // getCategories();
+        fetchVentes();
     }, []);
 
-    // Calculate Sold Products
-    const calculateSoldProducts = () => {
-        return products.filter((product) => product.solde).length;
+    const handleSearch = (e) => {
+        const value = e.target.value;
+        setSearch(value);
+
+        const filtered = ventes.filter((vente) =>
+            vente.numFacture.toLowerCase().includes(value.toLowerCase())
+        );
+        setFilteredVentes(filtered);
     };
 
-    // Calculate Categories Count
-    const calculateCategories = () => {
-        // Exclude "Tous les catégories" if present
-        return categories.filter((cat) => cat._id !== "Tous les catégories").length;
-    };
-
-    // Delete Product
-
-
-    // Show Delete Confirmation Modal
-    const showDeleteConfirm = (product) => {
-        confirm({
-            title: "Êtes-vous sûr de vouloir supprimer ce produit ?",
-            icon: <ExclamationCircleOutlined />,
-            content: `Produit : ${product.nom}`,
-            okText: "Oui",
-            okType: "danger",
-            cancelText: "Non",
-            onOk() {
-                // deleteProduct(product._id);
-            },
-            onCancel() {
-                // Optional: Handle cancel action
-            }
-        });
-    };
-
-    // Table Columns Configuration
-    const columns = [
-        // {
-        //   key: "id",
-        //   render: (_, __, index) => index + 1,
-        //   width: 50,
-        //   align: "center"
-        // },
-        {
-            title: "Photo",
-            dataIndex: "mainPicture",
-            render: (mainPicture) => (
-                <img
-                    src={`${process.env.REACT_APP_API_URL_PRODUCTION}${mainPicture}`}
-                    alt="mainPicture"
-                    style={{ width: "70px", height: "70px", borderRadius: "8px" }}
-                />
-            ),
-            width: 100,
-            align: "center"
-        },
-        {
-            title: "Nom du produit",
-            dataIndex: "nom",
-            sorter: (a, b) => a.nom.localeCompare(b.nom),
-            ellipsis: true
-        },
-        {
-            title: "Categorie",
-            dataIndex: "categorie"
-        },
-        {
-            title: "subCategorie",
-            dataIndex: "subCategorie"
-        },
-        // {
-        //   title: "Nombre de variants",
-        //   dataIndex: "variants",
-        //   render: (variant) => variant.length,
-        // },
-        {
-            title: "Prix",
-            dataIndex: "prix",
-            sorter: (a, b) => a.prix - b.prix,
-            render: (prix) => `${prix} TND`,
-            align: "right",
-            width: 100
-        },
-        {
-            title: "Nombre de variantes",
-            dataIndex: "variants",
-            render: (variants) => variants.length,
-            align: "center",
-            width: 150
-        },
-        {
-            title: "Soldé",
-            dataIndex: "solde",
-            render: (solde) => <Tag color={solde ? "green" : "red"}>{solde ? "Oui" : "Non"}</Tag>,
-            align: "center",
-            width: 100
-        },
-        {
-            title: "Action",
-            dataIndex: "action",
-            render: (_, product) => (
-                <Space size="middle">
-                    <Button
-                        type="link"
-                        icon={<EditOutlined />}
-                        onClick={() => navigate(`/produit/details/`, { state: { productId: product._id } })} // Navigate to DetailProduct with product ID
-                    />
-                    <Button
-                        type="link"
-                        danger
-                        icon={<DeleteOutlined />}
-                        onClick={() => showDeleteConfirm(product)}
-                    />
-                </Space>
-            ),
-            align: "center",
-            width: 100
+    const handleValidate = async (id) => {
+        console.log(`Valider la vente avec l'ID : ${id}`);
+        try {
+            const response = await axios.put(`${process.env.REACT_APP_API_URL_PRODUCTION}api/vente/validate/${id}`, {}, {
+                headers: { "x-api-key": process.env.REACT_APP_API_KEY },
+            });
+            console.log(response.data);
+            fetchVentes();
+        } catch (error) {
+            console.error(error);
         }
+    };
+
+    const columns = [
+        {
+            title: 'Numéro de Facture',
+            dataIndex: 'numFacture',
+            key: 'numFacture',
+            align: 'center',
+        },
+        {
+            title: 'Nombre de Produits',
+            dataIndex: 'products',
+            key: 'products',
+            align: 'center',
+            render: (products) => products.length,
+        },
+        {
+            title: 'Prix Total',
+            dataIndex: 'totalPrixVente',
+            key: 'totalPrixVente',
+            align: 'center',
+        },
+        {
+            title: 'Créé le',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+            align: 'center',
+            render: (date) => moment(date).format('YYYY-MM-DD HH:mm'),
+        },
+        {
+            title: 'Mis à jour le',
+            dataIndex: 'updatedAt',
+            key: 'updatedAt',
+            align: 'center',
+            render: (date) => moment(date).format('YYYY-MM-DD HH:mm'),
+        },
+        {
+            title: 'Validé',
+            dataIndex: 'isValidated',
+            key: 'isValidated',
+            align: 'center',
+            render: (isValidated) => (
+                <Tag color={isValidated ? 'green' : 'red'}>
+                    {isValidated ? 'Validé' : 'Non Validé'}
+                </Tag>
+            ),
+        },
+        {
+            title: 'Actions',
+            key: 'actions',
+            align: 'center',
+            render: (_, record) => (
+                <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
+                    <Button
+                        icon={<EyeOutlined />}
+                        onClick={() => navigate(`/vente/edit/${record._id}`)}
+                    >
+                        Voir
+                    </Button>
+                    {
+                        record.isValidated ? null : (
+                            <Button
+                                icon={<CheckCircleOutlined />}
+                                type="primary"
+                                onClick={() => handleValidate(record._id)}
+                            >
+                                Valider
+                            </Button>
+                        )
+                    }
+                </div>
+            ),
+        },
     ];
 
-    // Reset Filters Function
-    const resetFilters = () => {
-        setCategoryFilter("Tous les catégories");
-        setSearch("");
-        setIsSale(false);
-        setPriceFilter("asc");
-    };
-
-    // Navigation Handlers
-    const handleAddProduct = () => {
-        navigate("/produit/ajouter"); // Adjust the path as per your routing setup
-    };
+    if (loading) return <Spin tip="Chargement..." />;
+    if (error) return <Alert message="Erreur" description={error} type="error" showIcon />;
 
     return (
-        <div style={{ padding: "20px" }}>
-            <div style={{ paddingBottom: "25px" }}>
-                <Box className="breadcrumb">
-                    <Breadcrumb
-                        routeSegments={[
-                            { name: "Liste Des Vente", path: "/vente/list" },
-                            { name: "Liste des Vente" }
-                        ]}
-                    />
-                </Box>
+        <div style={{ padding: '20px' }}>
+            <h1 style={{ textAlign: 'center' }}>Liste des Ventes</h1>
+            <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+                <Input
+                    placeholder="Rechercher par Numéro de Facture"
+                    prefix={<SearchOutlined />}
+                    value={search}
+                    onChange={handleSearch}
+                    style={{ width: '50%' }}
+                />
             </div>
-
-            <Row gutter={16}>
-                <Col xs={24} sm={12} md={8}>
-                    <StatsCard title="Nombre Total des Produits" value={fullResponse.totalProducts || 0} />
-                </Col>
-                <Col xs={24} sm={12} md={8}>
-                    <StatsCard title="Nombre de Produits Soldés" value={calculateSoldProducts()} />
-                </Col>
-                <Col xs={24} sm={12} md={8}>
-                    <StatsCard title="Nombre de Catégories" value={calculateCategories()} />
-                </Col>
-                {/* <Col xs={24} sm={12} md={6}>
-          <StatsCard
-            title="Nombre de Sous-Catégories"
-            value={fullResponse.totalSubCategories || 0}
-          />
-        </Col> */}
-            </Row>
-
-            <Divider orientation="left">Nos Produits</Divider>
-
-            {/* Filters */}
-            {/* <Filters
-                search={search}
-                setSearch={setSearch}
-                categoryFilter={categoryFilter}
-                setCategoryFilter={setCategoryFilter}
-                categories={categories}
-                priceFilter={priceFilter}
-                setPriceFilter={setPriceFilter}
-                isSale={isSale}
-                setIsSale={setIsSale}
-                resetFilters={resetFilters}
-                onAddProduct={handleAddProduct} // Pass the navigation handler
-            /> */}
-
-            {/* Products Table */}
-            <div style={{ marginTop: "20px" }}>
-                <Spin spinning={loading}>
-                    {products.length > 0 ? (
-                        <Table
-                            columns={columns}
-                            dataSource={products.reverse()}
-                            rowKey="_id"
-                            pagination={{ pageSize: 10 }}
-                            bordered
-                            scroll={{ x: "max-content" }}
-                        />
-                    ) : (
-                        <div style={{ textAlign: "center", padding: "50px 0" }}>
-                            <p>Aucun produit trouvé.</p>
-                        </div>
-                    )}
-                </Spin>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Table
+                    dataSource={filteredVentes.map((vente) => ({ ...vente, key: vente._id }))}
+                    columns={columns}
+                    bordered
+                    pagination={{ pageSize: 10 }}
+                    style={{ width: '90%' }}
+                />
             </div>
         </div>
     );
--- src/app/Pages/Vente/Vente.jsx
+++ src/app/Pages/Vente/Vente.jsx
@@ -1,9 +1,269 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Form, Input, Button, Col, Row, Select, notification, Table } from "antd";
+import { PlusOutlined, DeleteOutlined } from "@ant-design/icons";
+import axios from "axios";
+
+const { Option } = Select;
 
 const Vente = () => {
-    return (
-        <div>Vente</div>
-    )
-}
+    const [form] = Form.useForm();
+    const [products, setProducts] = useState([]);
+    const [productEntries, setProductEntries] = useState([{ produit: null, variant: null, quantite: null, prixVente: null }]);
+    const [addedProducts, setAddedProducts] = useState([]);
+    const [totalPrixVente, setTotalPrixVente] = useState(0);
+    const [numFacture, setNumFacture] = useState("");
 
-export default Vente
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_API_URL_PRODUCTION}api/product/all/dashboard`, {
+                    headers: { "x-api-key": process.env.REACT_APP_API_KEY },
+                });
+                setProducts(response.data.products);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
+        fetchProducts();
+    }, []);
+
+    useEffect(() => {
+        const total = addedProducts.reduce((sum, product) => sum + (product.prixVente || 0) * (product.quantite || 0), 0);
+        setTotalPrixVente(total);
+    }, [addedProducts]);
+
+    const handleProductChange = (index, productId) => {
+        const selectedProduct = products.find((product) => product._id === productId);
+        const newProductEntries = [...productEntries];
+        newProductEntries[index].produit = productId;
+        newProductEntries[index].variantOptions = selectedProduct ? selectedProduct.variants : [];
+        newProductEntries[index].prixVente = selectedProduct ? selectedProduct.prixVente : null;
+        newProductEntries[index].variant = selectedProduct?.variants?.[0]?._id || null;
+        setProductEntries(newProductEntries);
+    };
+
+    const handleFieldChange = (index, field, value) => {
+        const newProductEntries = [...productEntries];
+        newProductEntries[index][field] = value;
+        setProductEntries(newProductEntries);
+    };
+
+    const handleAddProduct = () => {
+        const hasInvalidEntries = productEntries.some((entry) => !entry.quantite || entry.quantite <= 0);
+        if (hasInvalidEntries) {
+            notification.error({
+                message: "Erreur",
+                description: "Veuillez entrer une quantité valide pour tous les produits.",
+            });
+            return;
+        }
+
+        setAddedProducts([...addedProducts, ...productEntries]);
+        setProductEntries(productEntries.map((entry) => ({ ...entry, quantite: null })));
+    };
+
+    const handleQuantityChange = (index, newQuantity) => {
+        const updatedProducts = [...addedProducts];
+        updatedProducts[index].quantite = newQuantity;
+        setAddedProducts(updatedProducts);
+    };
+
+    const handleDeleteProduct = (index) => {
+        const updatedProducts = addedProducts.filter((_, i) => i !== index);
+        setAddedProducts(updatedProducts);
+    };
+
+    const handleSubmit = async () => {
+        try {
+            if (!numFacture) {
+                notification.error({ message: "Veuillez entrer un numéro de facture." });
+                return;
+            }
+
+            const productsData = addedProducts.map((product) => ({
+                variantId: product.variant,
+                quantite: product.quantite,
+            }));
+
+            const data = {
+                numFacture,
+                products: productsData,
+                totalPrixVente,
+            };
+
+            const response = await axios.post(`${process.env.REACT_APP_API_URL_PRODUCTION}api/vente/create`, data, {
+                headers: { "x-api-key": process.env.REACT_APP_API_KEY },
+            });
+
+            notification.success({ message: "Vente ajoutée avec succès!" });
+            setNumFacture("");
+            setAddedProducts([]);
+        } catch (error) {
+            console.error("Error submitting vente:", error);
+            notification.error({ message: "Erreur lors de l'ajout de la vente." });
+        }
+    };
+
+    const columns = [
+        {
+            title: "Produit",
+            dataIndex: "produit",
+            key: "produit",
+            render: (productId) => products.find((product) => product._id === productId)?.nom || "",
+        },
+        {
+            title: "Variant",
+            dataIndex: "variant",
+            key: "variant",
+            render: (variantId, record) => {
+                const product = products.find((product) => product._id === record.produit);
+                const variant = product?.variants?.find((variant) => variant._id === variantId);
+                return variant?.reference || "N/A";
+            },
+        },
+        {
+            title: "Quantité",
+            dataIndex: "quantite",
+            key: "quantite",
+            render: (quantite, record, index) => (
+                <Input
+                    type="number"
+                    value={quantite}
+                    onChange={(e) => handleQuantityChange(index, parseInt(e.target.value) || 0)}
+                />
+            ),
+        },
+        {
+            title: "Prix Unitaire",
+            dataIndex: "prixVente",
+            key: "prixVente",
+            render: (prixVente) => (prixVente ? `${prixVente}` : "N/A"),
+        },
+        {
+            title: "Prix Total",
+            key: "prixTotal",
+            render: (text, record) =>
+                record.prixVente && record.quantite
+                    ? `${record.prixVente * record.quantite}`
+                    : "N/A",
+        },
+        {
+            title: "Action",
+            key: "action",
+            render: (text, record, index) => (
+                <Button
+                    danger
+                    icon={<DeleteOutlined />}
+                    onClick={() => handleDeleteProduct(index)}
+                >
+                    Supprimer
+                </Button>
+            ),
+        },
+    ];
+
+    return (
+        <div style={{ padding: "20px", maxWidth: "1200px", margin: "50px auto", backgroundColor: "white", borderRadius: "8px" }}>
+            <h2 style={{ marginBottom: "20px" }}>Créer une Nouvelle Vente</h2>
+            <Form form={form} layout="vertical" name="add_products_form">
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Form.Item label="Numéro de Facture" required>
+                            <Input
+                                placeholder="Entrer le numéro de facture"
+                                value={numFacture}
+                                onChange={(e) => setNumFacture(e.target.value)}
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <h3>Produits Ajoutés</h3>
+                {productEntries.map((entry, index) => (
+                    <Row gutter={16} align="middle" key={index} style={{ marginBottom: "16px" }}>
+                        <Col span={8}>
+                            <Form.Item label="Produit" required>
+                                <Select
+                                    showSearch
+                                    placeholder="Sélectionner un produit"
+                                    onChange={(value) => handleProductChange(index, value)}
+                                    value={entry.produit}
+                                    optionFilterProp="children"
+                                    filterOption={(input, option) =>
+                                        option?.children.toLowerCase().includes(input.toLowerCase())
+                                    }
+                                >
+                                    {products.map((product) => (
+                                        <Option key={product._id} value={product._id}>
+                                            {product.nom}
+                                        </Option>
+                                    ))}
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={8}>
+                            <Form.Item label="Variant" required>
+                                <Select
+                                    placeholder="Sélectionner un variant"
+                                    onChange={(value) => handleFieldChange(index, "variant", value)}
+                                    value={entry.variant}
+                                    disabled={!entry.variantOptions}
+                                >
+                                    {entry.variantOptions &&
+                                        entry.variantOptions.map((variant) => (
+                                            <Option key={variant._id} value={variant._id}>
+                                                {variant.reference}
+                                            </Option>
+                                        ))}
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={5}>
+                            <Form.Item label="Quantité" required>
+                                <Input
+                                    placeholder="Quantité"
+                                    type="number"
+                                    onChange={(e) => handleFieldChange(index, "quantite", parseInt(e.target.value) || null)}
+                                    value={entry.quantite}
+                                />
+                            </Form.Item>
+                        </Col>
+                        <Col span={3} style={{ textAlign: "center" }}>
+                            <Button
+                                type="primary"
+                                icon={<PlusOutlined />}
+                                onClick={handleAddProduct}
+                                style={{ marginTop: "24px" }}
+                            >
+                                Ajouter
+                            </Button>
+                        </Col>
+                    </Row>
+                ))}
+            </Form>
+
+            <Table
+                columns={columns}
+                dataSource={addedProducts.map((product, index) => ({ ...product, key: index }))}
+                pagination={false}
+                style={{ marginTop: "20px" }}
+            />
+            <div style={{ textAlign: "right", marginTop: "20px", }}>
+                <div><b>Total Prix Vente:</b> {totalPrixVente} €</div>
+                <Button
+                    type="primary"
+                    onClick={handleSubmit}
+                    style={{
+                        backgroundColor: "#f9f9f9",
+                        color: "#1890ff",
+                        border: "1px solid #1890ff",
+                    }}
+                >
+                    Soumettre les Modifications
+                </Button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Vente;
